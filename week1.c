72e40a745c22bc78a3bb8ebc1cb44e14240189d3
Tata Thanapat <thanapat.war@ku.th>
2020-02-09 19:36:55 +0700

    type ต่างๆของตัวแปร

---
+++ week1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define bb "asdfgh" 
+int main(){
+
+    long long int a = 4000000000; 
+    unsigned int b = 400000000; 
+    char c = 'a';
+    char d = 97;
+    float f = 0.1245;
+    double e = 20.18888;
+    float j;
+    char p[] = "ABC"; 
+    printf("%10s" , p); 
+    char r = '\0';
+
+}
